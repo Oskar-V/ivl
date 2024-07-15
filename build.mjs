029be21133e1963f3de052c34f2fc672d4809102
Oskar <oskar@voorel.com>
2024-07-15 18:48:11 +0300

    fix sub folder building

--- build.mjs
+++ build.mjs
@@ -1,8 +1,22 @@
 import dts from 'bun-plugin-dts'
 
 await Bun.build({
-	entrypoints: ['./src/index.ts', './src/patterns/index.ts', './src/helpers/index.ts'],
+	entrypoints: ['./src/index.ts'],
 	outdir: './lib',
+	minify: true,
+	plugins: [dts()]
+})
+
+await Bun.build({
+	entrypoints: ['./src/patterns/index.ts'],
+	outdir: './lib/patterns',
+	minify: true,
+	plugins: [dts()]
+})
+
+await Bun.build({
+	entrypoints: ['./src/helpers/index.ts'],
+	outdir: './lib/helpers',
 	minify: true,
 	plugins: [dts()]
 })
