b8ef53431b32393f697a2bc66ee7286c124d3384
Oskar <oskar@voorel.com>
2024-07-15 18:49:03 +0300

    separate helpers based on usage target

---
+++ src/helpers/rules.ts
@@ -0,0 +1,23 @@
+// Functions which affect a single rule
+
+import { RULE } from '@types';
+
+export const type =
+	(type: "string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function"): RULE =>
+		(value: unknown) => typeof value === type
+
+export const matchesRegex = (regex: RegExp) =>
+	(value: unknown) =>
+		typeof value === 'string' && regex.test(value)
+
+export const stringBetween = (max = 100, min = 0) =>
+	(s: unknown) =>
+		typeof s === 'string' && s.length >= min && s.length < max
+
+export const numberBetween = (max = 100, min = 1) =>
+	(i: unknown) =>
+		typeof i === 'number' && i >= min && i < max
+
+export const acceptAny = (rules: RULE[] = []) =>
+	async (i: unknown, ...extra: unknown[]) =>
+		(await Promise.all(rules.map(rule => rule.call(undefined, i, ...extra)))).some(e => e)
---
+++ src/helpers/schema.ts
@@ -0,0 +1,13 @@
+// Functions which affect the a whole rule set - to be used inside schema objects
+
+import { RULES } from '@types'
+
+export const allowUndefined = (rules: RULES) =>
+	Object.entries(rules).reduce((acc, [key, rule]) => ({
+		...acc, [key]: (i: unknown, ...overload: unknown[]) => typeof i === 'undefined' ? true : rule(i, ...overload)
+	}), {});
+
+export const preprocess = (fn: Function, rules: RULES) =>
+	Object.entries(rules).reduce((acc, [key, rule]) => ({
+		...acc, [key]: (i: unknown, ...overload: unknown[]) => rule(fn(i), ...overload)
+	}), {})
